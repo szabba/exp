a83e8250f80cc1645784224ff9a22bc8526c9934
Karol Marcjan <karol.marcjan@gmail.com>
2017-05-17 19:40:33 +0200

    doggy: Change the coordinate system we draw in

--- doggy/game/render.go
+++ doggy/game/render.go
@@ -12,11 +12,13 @@ type State interface {
 }
 
 func Draw(ctx draw2d.GraphicContext, state State, width, height float64) {
+	ctx.Translate(width/2, height)
+	ctx.Scale(1, -1)
 	ctx.Save()
 	defer ctx.Restore()
 
 	r := 100.0
-	cx, cy := width/2, height/2
+	cx, cy := 0.0, 200.0
 
 	ctx.BeginPath()
 	ctx.SetStrokeColor(color.Black)
