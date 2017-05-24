4bad4c1439daedd1182815abb08e332938a1124c
Karol Marcjan <karol.marcjan@gmail.com>
2017-05-24 18:19:08 +0200

    narrative/simple_world_state: Move away from a dict model in ui

--- narrative/simple_world_state/ui.rkt
+++ narrative/simple_world_state/ui.rkt
@@ -1,201 +1,347 @@
 #lang rackjure
 
+
 (require srfi/1)
 (require "termprog.rkt")
 (require "world-state.rkt")
+
 
 (provide (contract-out [init (-> world-state? program?)]
                        [run-world! (-> world-state? void?)]))
 
 
+;;;; Types
+;; TODO: use non-dictionary for model
+
+
+;; TODO: debug log
+#;(struct debug-log
+    (qualities covered-state)
+    #:transparent)
+
+
+(struct log-n-prompt
+  (world log prompt scroll)
+  #:transparent)
+
+
+(struct done
+  (last-state)
+  #:transparent)
+
+
+#;(type-alias Model
+    (U LogNPrompt
+       (Done Model)))
+
+
+;;;; Boilerplate
+
+
+#;(-> World Void)
 (define (run-world! world)
   (run! (init world)))
 
+
+#;(-> World (Program Model))
 (define (init world)
   (program (init-model world) update view exit?))
 
-(define (init-model world)
-  (~> {'world world 'scroll 'prompt 'options (~>> world world->options-focus) 'log (list) 'upscroll 0 'done #f}))
 
-(define (world->options-focus world)
+#;(-> World Model)
+(define (init-model world)
+  (log-n-prompt world (list) (~> world world->prompt) 'prompt))
+
+;;;; Helpers
+
+
+#;(-> WorldState (Focus Option))
+(define (world->prompt world)
   (match (~>> world world-state-available-options)
     [(list) (list)]
     [(list head rest ...)
      (focus (list) head rest)]))
 
 
+#;(-> String x x)
+(define (debug/log prefix v)
+  (display prefix)
+  (display ": ")
+  (print v)
+  (display "\r")
+  (newline)
+  v)
+
+
+;;;; Update
+
+
+#;(-> Key Model Model)
 (define (update key model)
-  (match (list (model 'scroll) key)
-    [(list _ 'escape) (model 'done #t)]
-    [(list _ #\q) (model 'done #t)]
+  (debug/log "update: key" key)
+  (debug/log "update: model" model)
 
-    [(list _ 'pgup) (~> model switch-scroll-area)]
-    [(list _ 'pgdn) (~> model switch-scroll-area)]
+  (match (list model key)
+    [(list (done _) _) model]
 
-    [(list 'prompt 'up)
+    [(list _ #\q) (done model)]
 
+    [(list (log-n-prompt _ _ _ scroll-area) 'tab)
+     (struct-copy log-n-prompt model
+                  (scroll (~> scroll-area switch-scroll-area)))]
+
+    [(list (log-n-prompt _ log _ 'log) 'up)
+     (struct-copy log-n-prompt model
+                  (log
+                   (~>> log
+                       (log-scroll focus-shift-up))))]
+
+    [(list (log-n-prompt _ log _ 'log) 'down)
+     (struct-copy log-n-prompt model
+                  (log
+                   (~>> log
+                       (log-scroll focus-shift-down))))]
+
+    [(list (log-n-prompt _ _ prompt 'prompt) 'up)
+     (struct-copy log-n-prompt model
+                  (prompt
+                   (~>> prompt
+                        (shift-options focus-shift-up))))]
+
+    [(list (log-n-prompt _ _ prompt 'prompt) 'down)
+     (struct-copy log-n-prompt model
+                  (prompt
+                   (~>> prompt
+                        (shift-options focus-shift-down))))]
+
+    [(list (log-n-prompt _ _ _ 'prompt) 'return)
      (~>> model
-          'options
-          (shift-options focus-shift-up)
-          (dict-set model 'options))]
-
-    [(list 'prompt 'down)
-
-     (~>> model
-          'options
-          (shift-options focus-shift-down)
-          (model 'options))]
-    
-    [(list 'prompt 'return)
-     (~> model
          log-focused-option
+         (model-scroll-log focus-scroll-down)
          apply-focused-option
-         fix-up-options)]
+         fix-up-prompt)]
 
-    [(list 'log 'up)
-     
-     (~>> model
-          (add-to-upscroll 1)
-          fix-upscroll-range)]
-
-    [(list 'log 'down)
-     (~>> model
-          (add-to-upscroll -1)
-          fix-upscroll-range)]
-    
     [_ model]))
 
-(define (add-to-upscroll n model)
-  (~>> model
-       'upscroll
-       (+ n)
-       (model 'upscroll)))
 
-(define (fix-upscroll-range model)
-  (define upscroll (model 'upscroll))
-  (define log-len (~>> model 'log length))
-  
-  (~>> (cond ((< upscroll 0) 0)
-             ((<= log-len upscroll) (- log-len 1))
-             (else upscroll))
-       (model 'upscroll)))
+#;(-> (-> (Focus x) (Focus x)) LogNPrompt LogNPrompt)
+(define (model-scroll-log f model)
+  (struct-copy log-n-prompt model
+               (log
+                (~> model log-n-prompt-log f))))
 
-(define (switch-scroll-area model)
-  (model 'scroll
-         (match (model 'scroll)
-           ['log 'prompt]
-           ['prompt 'log])))
 
+#;(-> (-> (Focus x) (Focus x)) (U Null (Focus x)))
+(define (log-scroll dir-f log)
+  (match log
+    [(list) log]
+
+    [(focus _ _ _) (~> log dir-f)]))
+
+
+#;(-> (U 'log 'prompt) (U 'log 'prompt))
+(define (switch-scroll-area scroll-area)
+  (match scroll-area
+    ['log 'prompt]
+    ['prompt 'log]))
+
+
+#;(-> (-> (Focus x) (Focus x)) (U Null (Focus x)))
 (define (shift-options dir opts)
   (match opts
     [(list) (list)]
     [(focus _ _ _) (dir opts)]))
 
+
+#;(-> Model Model)
 (define (log-focused-option model)
-  
-  (match (model 'options)
-    [(list) model]
+  (match model
+    [(log-n-prompt _ _ (list) _) model]
 
-    [(focus _ opt _)
-     (let [(log (model 'log))]
+    [(log-n-prompt _ log (focus _ opt _) _)
 
-       (~>> (lines-for-option opt (first-entry? log))
-            (append log)
-            (model 'log)))]))
+     (struct-copy log-n-prompt model
+                  (log (~>> opt
+                           (lines-for-option #:first-entry? (null? log))
+                           (append-to-log log))))]))
 
-(define (lines-for-option opt first-entry?)
+
+#;(-> (U Null (Focus String)) (List String) (U Null (Focus String)))
+(define (append-to-log log lines)
+
+  (match (list log lines)
+    [(list _ (list)) log]
+
+    [(list (list) lines)
+     (focus (drop-right lines 1) (last lines) (list))]
+
+    [(list (focus _ _ _) lines)
+     (focus-append-below log lines)]))
+
+
+#;(-> Option #:first-entry? Boolean (List String))
+(define (lines-for-option opt #:first-entry? [firts? #f])
+
+  (define (indent line)
+    (string-append "    " line))
+
   (match opt
     [(option title _ body-text _)
 
      (let [(lines (append (list title)
                           (list "")
-                          (~>> body-text
-                               
-                               (map (λ (line) (string-append "    " line))))))]
+                          (~>> body-text (map indent))))]
 
        (if first-entry?
            lines
            (append (list "") lines)))]))
 
+
+#;(-> x Boolean)
 (define first-entry? null?)
 
+
+#;(-> LogNPrompt LogNPrompt)
 (define (apply-focused-option model)
-  
-  (let [(world (model 'world))
-        (opts (model 'options))]
-    
-    (match opts
-      [(list) model]
-      [(focus _ opt _)
-       (~>> opt
-            (world-state-apply-option world)
-            (model 'world))])))
-
-(define (fix-up-options model)
-  (~>> (model 'world)
-       world->options-focus
-       (model 'options)))
+  (match model
+    [(log-n-prompt _ _ (list) _) model]
+    [(log-n-prompt world
+     _ (focus _ opt _) _)
+      (struct-copy log-n-prompt model
+                  (world
+                    (~>> opt
+                        (world-state-apply-option world))))]))
 
 
+#;(-> LogNPrompt LogNPrompt)
+(define (fix-up-prompt model)
+  (struct-copy log-n-prompt model
+               (prompt
+                (~>> model
+                     log-n-prompt-world
+                     world->prompt))))
+
+
+;;;; View
+
+
+#;(-> Int Int Model (List String))
 (define (view w h model)
+  (match model
 
-  (define log (model 'log))
-  (define log-len (length log))
+    [(done last)
+     (view w h last)]
+
+    [(log-n-prompt _ log prompt _)
+     (view/log-n-prompt w h log prompt)]))
+
+
+#;(-> Int Int (U Null (Focus String)) (U Null (Focus Option)))
+(define (view/log-n-prompt w h log prompt)
 
   (define bottom
     (list (make-string w #\-)
-          (current-prompt model)))
-  
+          (current-prompt-line prompt)))
+
   (define log-space
     (- h (length bottom)))
-  
-  (append (visible-log log-space model) bottom))
 
-(define (visible-log log-space model)
-  (define log (model 'log))
-  (define upscroll (model 'upscroll))
-  
+  (append (visible-log log-space log)
+          bottom))
+
+
+#;(-> Int (U Null (Focus String) (List String)))
+(define (visible-log log-space log)
   (~> log
-      (drop-right-upto upscroll)
-      (take-right-upto log-space)
+      log-above-prompt
       (prefix-with-upto log-space "")))
 
+
+#;(-> (U Null (Focus String) (List String)))
+(define (log-above-prompt log)
+  (~> (match log
+        [(list) (list)]
+        [(focus above curr _) (cons curr above)])
+      reverse))
+
+
+#;(-> (List String) Int String (List String))
 (define (prefix-with-upto l n p)
   (~> (append (make-list n p) l)
       (take-right-upto n)))
 
-(define (drop-right-upto l n)
+
+#;(-> (List String) Int (List String))
+#;(define (drop-right-upto l n)
   (~>> n
        (min (length l))
        (drop-right l)))
 
+
+#;(-> (List String) Int (List String))
 (define (take-right-upto l n)
   (~>> n
        (min (length l))
        (take-right l)))
 
-(define (current-prompt model)
-  (match (model 'options)
 
-    [(list)
-     "! No actions available."]
+#;(-> (U Null (Focus Option) String))
+(define (current-prompt-line prompt)
+  (match prompt
+
+    [(list) "! No actions available."]
 
     [(focus _ (option title _ _ _) _)
      (string-append "> " title)]))
 
+
+#;(-> Model Boolean)
 (define (exit? model)
-  (model 'done))
+  (match model
+    [(done _) #t]
+    [_ #f]))
+
+;;;; Focus (a non-empty list zipper)
+
 
 (struct focus (above on below) #:transparent)
 
+
+#;(-> (Focus x) (Focus x))
 (define (focus-shift-up foc)
   (match foc
     [(focus (list) _ _) foc]
     [(focus (list h t ...) m b)
      (focus t h (cons m b))]))
 
+
+#;(-> (Focus x) (Focus x))
 (define (focus-shift-down foc)
   (match foc
     [(focus _ _ (list)) foc]
     [(focus a m (list h t ...))
      (focus (cons m a) h t)]))
+
+
+#;(-> (Focus x) (Focus x))
+(define (focus-scroll-down foc)
+  (match foc
+    [(focus _ _ (list)) foc]
+    [(focus a c b)
+     (focus (append (drop-right b 1) (cons c a))
+            (last b)
+            (list))]))
+
+
+#;(-> (Focus x) (Focus x))
+#;(define (focus-append-above foc aboves)
+  (match foc
+    [(focus a c b)
+     (focus (append (reverse aboves) a) c b)]))
+
+
+#;(-> (Focus x) (Focus x))
+(define (focus-append-below foc belows)
+  (match foc
+    [(focus a c b)
+     (focus a c (append b belows))]))
